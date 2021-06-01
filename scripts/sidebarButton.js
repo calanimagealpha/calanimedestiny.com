ca31fb932a26b73ece2d1f18910edbee8804975e
girantinas <girantinas@github.com>
2021-06-01 01:02:48 -0400

    added scripts folder for js

---
+++ scripts/sidebarButton.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mainSidebar").style.width = "250px";
+    document.getElementById("sidebarSlidContent").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mainSidebar").style.width = "0";
+    document.getElementById("sidebarSlidContent").style.marginLeft = "0";
+} 
